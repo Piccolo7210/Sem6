75304c352693e0d0e9c00c62d3d6b178139dc82b
Piccolo7210 <mustakim.mohsin7210@gmail.com>
2025-05-04 11:11:33 +0600

    UserManagement Added

--- SoftwareDesign/usermanagement/src/test/java/org/example/usermanagement/UserManagementApplicationTests.java
+++ SoftwareDesign/usermanagement/src/test/java/org/example/usermanagement/UserManagementApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.usermanagement;
+package org.example.usermanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UsermanagementApplicationTests {
+class UserManagementApplicationTests {
 
     @Test
     void contextLoads() {
