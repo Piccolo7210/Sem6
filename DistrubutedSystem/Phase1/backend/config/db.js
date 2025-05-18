306d567c245ce1640ba686eb5a8dda30872bfc76
Mustakim Bin Mohsin <114597646+Piccolo7210@users.noreply.github.com>
2025-05-18 11:28:04 +0600

    Update db.js

--- DistrubutedSystem/Phase1/backend/config/db.js
+++ DistrubutedSystem/Phase1/backend/config/db.js
@@ -9,5 +9,8 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
+MONGODB_URI=mongodb+srv://bsse1405:YRJDa6XUXZG3PrZp@cluster0.sntvef1.mongodb.net/library_system
+PORT=3100
+JWT_SECRET=your_jwt_secret_key_here
 
 export default connectDB;
